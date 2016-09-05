d29be201f55065652bb6b3998b2216271777df43
robertsikora <robertsikora@interia.pl>
2016-09-05 18:22:38 +0200

    test

--- id-server/src/main/java/IdServiceApplication.java
+++ id-server/src/main/java/IdServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class IdServiceApplication {
 
     public static void main(String[] args) {
+        System.out.printf("aaa");
         SpringApplication.run(IdServiceApplication.class, args);
     }
 }
--- id-server/src/main/java/id/server/IdResourceImpl.java
+++ id-server/src/main/java/id/server/IdResourceImpl.java
@@ -5,8 +5,13 @@ import id.domain.Account;
 import id.domain.Credentials;
 import id.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by robertsikora on 26.03.2016.
@@ -16,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 class IdResourceImpl implements IdResource {
 
     private final AccountService accountService;
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     public IdResourceImpl(final AccountService accountService) {
@@ -30,5 +38,13 @@ class IdResourceImpl implements IdResource {
     @Override
     public void register(@RequestBody final Account account) {
         accountService.register(account);
+    }
+
+    @Override
+    public ResponseEntity test() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://www.onet.pl?param=xxx");
+        headers.add("token", "bla bla bla 123");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
